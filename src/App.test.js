f1c6c6c7585bb11ea611296310c525a0a6cc8c90
Himanshu <himanshuparodiya@gmail.com>
2023-11-01 15:54:02 +0530

    Run our first test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+// Import the 'render' and 'screen' functions from the '@testing-library/react' library.
 import { render, screen } from '@testing-library/react';
+
+// Import the 'App' component from the './App' file.
 import App from './App';
 
-test('renders learn react link', () => {
+// Define a test case with a description.
+test('Check learn react text in app', () => {
+  // Render the 'App' component to the virtual DOM for testing purposes.
   render(<App />);
+
+  // Use the 'screen' object to find an element with text matching 'learn react' (case-insensitive).
   const linkElement = screen.getByText(/learn react/i);
+
+  // Expect that the 'linkElement' is in the document (i.e., it should be present).
   expect(linkElement).toBeInTheDocument();
 });
