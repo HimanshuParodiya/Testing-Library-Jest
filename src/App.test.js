0b0841c1489ea4905c6a4ad19d8b709e813098dd
Himanshu <himanshuparodiya@gmail.com>
2024-01-17 11:46:48 +0530

    Write first react test case + case sensitivity

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,11 @@
-// Import the 'render' and 'screen' functions from the '@testing-library/react' library.
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-// Import the 'App' component from the './App' file.
-import App from './App';
-
-// Define a test case with a description.
-test('Check learn react text in app', () => {
-  // Render the 'App' component to the virtual DOM for testing purposes.
+test('Test First React App Case', () => {
   render(<App />);
+  const text = screen.getByText(/first react Test case/i)
+  const imageTitle = screen.getByTitle(/Ai Generated image/i)
 
-  // Use the 'screen' object to find an element with text matching 'learn react' (case-insensitive).
-  const linkElement = screen.getByText(/learn react/i);
-
-  // Expect that the 'linkElement' is in the document (i.e., it should be present).
-  expect(linkElement).toBeInTheDocument();
-});
+  expect(text).toBeInTheDocument();
+  expect(imageTitle).toBeInTheDocument();
+})
--- src/Sum.test.js
+++ src/Sum.test.js
@@ -4,5 +4,5 @@ test('Check for sum', () => {
     expect(Sum(10, 10)).toBe(20);
 })
 test('Check for sum 2', () => {
-    expect(Sum(100, 10)).toBe(160);
+    expect(Sum(100, 10)).toBe(110);
 })
